693ac048b81ef2dbb0c967a603fc7bf372cfd209
krisitrendafilova <kristina.trendafilova15@gmail.com>
2025-03-10 08:49:59 +0200

    Student project

---
+++ src/main/java/collection/Storage.java
@@ -0,0 +1,37 @@
+package collection;
+import models.Student;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
+    private static Storage instance = null;
+    private List<Student> students;
+
+    public Storage() {
+        students = new ArrayList<>();
+    }
+
+    public List<Student> getStudent() {
+        return new ArrayList<>(students);
+    }
+
+    public static Storage getInstance() {
+        if (instance == null) {
+            instance = new Storage();
+        }
+        return instance;
+    }
+
+    public void addVote(Student st) {
+        // Проверяваме дали студент със същия fNumber вече съществува
+        for (Student student : students) {
+            if (student.getfNumber().equals(st.getfNumber())) {
+                student.setVote(); // Увеличаваме гласа
+                return; // Излизаме от метода
+            }
+        }
+        // Ако не е намерен студент със същия fNumber, добавяме новия
+        students.add(st);
+    }
+
+}
---
+++ src/main/java/servlets/AddVoteServlet.java
@@ -0,0 +1,46 @@
+package servlets;
+
+import collection.Storage;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import models.Student;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/vote")
+public class AddVoteServlet extends HttpServlet {
+    private final Storage storage = Storage.getInstance();
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head><title>Students:</title></head>");
+        out.println("<body>");
+
+        String fNumber = request.getParameter("fNumber");
+        String name = request.getParameter("name");
+        String lastName = request.getParameter("lastName");
+        String speciality = request.getParameter("speciality");
+
+
+        if (fNumber == null || fNumber.isEmpty() || name == null || name.isEmpty() || lastName == null || lastName.isEmpty()
+                || speciality == null || speciality.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            out.println("<p>All fields are required!</p>");
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+            Student student = new Student(fNumber, name, lastName, speciality ) ;
+            storage.addVote(student);
+            out.println("<p>Student is added successfully!</p>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
